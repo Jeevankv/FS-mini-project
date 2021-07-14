72062d1640eb68ee073a444aa1d023662436dfb3
Jeevan <jeevankv18@gmail.com>
2021-07-14 19:31:29 +0530

    added Login and register page

--- Library_Management/LibraryApp/urls.py
+++ Library_Management/LibraryApp/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('', views.login, name= 'library-login'),
     path('register/', views.register, name = 'library-register'),
     path('adminlogin/', views.adminLogin, name = 'library-admin'),
+    path('adminhome/', views.adminhome, name = 'library-adminhome'),
+    
 ]
--- Library_Management/LibraryApp/views.py
+++ Library_Management/LibraryApp/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 def login(request):
     return render(request, 'login.html')
 
 def register(request):
+
+    if request.method == 'POST':
+            data = request.POST
+            print(data)  
     return render(request, 'register.html')
 
+
+
+
 def adminLogin(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if email == 'jeevankv18@gmail.com' and password == 'admin':
+            return redirect('library-adminhome')
     return render(request, 'adminlogin.html')
+
+
+
+
+
+def adminhome(request):
+    return render(request, 'adminhome.html')
